502d81a746a5eed6c5ec1ed3c7ebf7d951efb932
fstawicki <stawickifilip1@gmail.com>
2022-09-26 20:21:17 +0200

    basic template

---
+++ src/components/Game.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Game = () => {
+  return (
+    <div>
+        <div className="container">
+            <img src="" alt="" className="image" />
+            <p className="countryname"></p>
+            <p className="city">Capital City: </p>
+        </div>
+    </div>
+  )
+}
+
+export default Game
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar_left">
+            <h1>Flag Guesser</h1>
+        </div>
+        <div className="navar_right">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
