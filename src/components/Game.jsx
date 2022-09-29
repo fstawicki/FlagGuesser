fdb5a5d52d40e8f0d9cc514d02d7ab24251eceda
fstawicki <stawickifilip1@gmail.com>
2022-09-29 12:54:53 +0200

    working on logic

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Game.scss'
 
-import albania from '../assets/albania.png'
+import countries from '../data/countries';
 
 const Game = () => {
+
+  const [isOn, setIsOn] = useState(false);
+  const [country, setCountry] = useState(countries[Math.floor(Math.random() * (20 - 0 + 1)) + 0]);
+  const [flag, setFlag] = useState('');
+
+  const getCountry = () => {
+
+    setIsOn(true);
+    setCountry((prevState) => countries[Math.floor(Math.random() * (20 - 0 + 1)) + 0]);
+    setFlag(`https://countryflagsapi.com/png/${country.code}`);
+
+  }
+
+
   return (
     <div className='game'>
         <div className="container">
-            <img src={albania} alt="flag" className="image" />
-            <p className="countryname">Country: Albania</p>
-            <p className="city">Capital City: Tirana</p>
+          {!isOn && <button className="button next" onClick={getCountry}>Start</button>}
+          {isOn && (
+            <>
+            <img src={flag} alt="flag" className="image" />
+            <p className="countryname">{country.name}</p>
+            <p className="city">{country.capital}</p>
             <button className="button">1</button>
             <button className="button">2</button>
             <button className="button">3</button>
             <button className="button">4</button>
-            <button className="button next">Next</button>
+            <button className="button next" onClick={getCountry}>Next</button>
+            </>
+          )}
         </div>
     </div>
   )
